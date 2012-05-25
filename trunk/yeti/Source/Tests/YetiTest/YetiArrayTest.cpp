34baaafa764c17f5bc720395c48f307854cab180
rainleaf2012@gmail.com <rainleaf2012@gmail.com@2e08af89-c0c4-1572-9ecc-6804e04b3515>
2012-05-25 06:25:40 +0000

    添加数组接口

--- trunk/yeti/Source/Tests/YetiTest/YetiArrayTest.cpp
+++ trunk/yeti/Source/Tests/YetiTest/YetiArrayTest.cpp
@@ -2,19 +2,23 @@
 
 #include "Yeti.h"
 
-NAMEBEG
-class A
+USINGNAMESPACE2;
+
+void test_array()
 {
-};
-NAMEEND
+    Array<int> aint;
+}
 
 int array_test(std::vector<std::string> args)
 {
     size_t argc = args.size();
     for (size_t i = 0; i < argc; ++i)
     {
+        if (i == 0) continue;
         std::cout << "Param[" << i << "] : " << args[i] << std::endl;
     }
+
+    test_array();
 
     return 0;
 }
--- trunk/yeti/Source/Tests/YetiTest/YetiCommonTest.cpp
+++ trunk/yeti/Source/Tests/YetiTest/YetiCommonTest.cpp
@@ -87,6 +87,7 @@ int common_test(std::vector<std::string> args)
     size_t argc = args.size();
     for (size_t i = 0; i < argc; ++i)
     {
+        if (i == 0) continue;
         std::cout << "Param[" << i << "] : " << args[i] << std::endl;
     }
 

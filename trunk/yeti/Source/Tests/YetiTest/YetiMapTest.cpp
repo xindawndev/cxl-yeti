a71be9ae829eb15ed737fa839f87c7dcda0c5cca
rainleaf2012@gmail.com <rainleaf2012@gmail.com@2e08af89-c0c4-1572-9ecc-6804e04b3515>
2012-05-29 14:07:33 +0000

    添加Map测试文件

---
+++ trunk/yeti/Source/Tests/YetiTest/YetiMapTest.cpp
@@ -0,0 +1,61 @@
+#include "Common.h"
+
+#include "Yeti.h"
+
+USINGNAMESPACE2;
+
+static unsigned int g_count = 0;
+
+class A {
+public:
+    A() : a_(0), b_(0), c_(&a_) {
+        printf("A::A()\n");
+        g_count++;
+    }
+
+    A(int a, char b) : a_(a), b_(b), c_(&a_) {
+        printf("A::A(%d, %d)\n", a, b);
+        g_count++;
+    }
+
+    A(const A & other) : a_(other.a_), b_(other.b_), c_(&a_) {
+        printf("A::A(copy: a=%d, b=%d)\n", a_, b_);
+        g_count++;
+    }
+
+    ~A() {
+        printf("A::~A(), a=%d, b=%d\n", a_, b_);
+        g_count--;
+    }
+
+    bool check() { return c_ == &a_;}
+
+    bool operator==(const A & other) const {
+        return a_ == other.a_ && b_ == other.b_;
+    }
+
+    int a_;
+    char b_;
+    int * c_;
+};
+
+int test_map()
+{
+    return 0;
+}
+
+int map_test(std::vector<std::string> args)
+{
+    size_t argc = args.size();
+    for (size_t i = 0; i < argc; ++i)
+    {
+        if (i == 0) continue;
+        std::cout << "Param[" << i << "] : " << args[i] << std::endl;
+    }
+
+    test_map();
+
+    return 0;
+}
+
+static TestRegister test("map_test", map_test);

5252d9b291987183d8313629fe2de87bb6433a01
rainleaf2012@gmail.com <rainleaf2012@gmail.com@2e08af89-c0c4-1572-9ecc-6804e04b3515>
2012-05-29 13:27:16 +0000

    添加Stack测试程序

---
+++ trunk/yeti/Source/Tests/YetiTest/YetiStackTest.cpp
@@ -0,0 +1,42 @@
+#include "Common.h"
+
+#include "Yeti.h"
+
+USINGNAMESPACE2;
+
+int test_stack()
+{
+    Stack<int> i_stack;
+    int i = 0;
+    CHECK(YETI_FAILED(i_stack.pop(i)));
+    CHECK(YETI_FAILED(i_stack.peek(i)));
+    CHECK(YETI_SUCCEEDED(i_stack.push(4)));
+    CHECK(YETI_SUCCEEDED(i_stack.push(5)));
+    CHECK(YETI_SUCCEEDED(i_stack.push(6)));
+    CHECK(YETI_SUCCEEDED(i_stack.pop(i)));
+    CHECK(i == 6);
+    CHECK(YETI_SUCCEEDED(i_stack.peek(i)));
+    CHECK(i == 5);
+    CHECK(YETI_SUCCEEDED(i_stack.pop(i)));
+    CHECK(i == 5);
+    CHECK(YETI_SUCCEEDED(i_stack.pop(i)));
+    CHECK(i == 4);
+
+    return 0;
+}
+
+int stack_test(std::vector<std::string> args)
+{
+    size_t argc = args.size();
+    for (size_t i = 0; i < argc; ++i)
+    {
+        if (i == 0) continue;
+        std::cout << "Param[" << i << "] : " << args[i] << std::endl;
+    }
+
+    test_stack();
+
+    return 0;
+}
+
+static TestRegister test("stack_test", stack_test);

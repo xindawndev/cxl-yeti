eab14a863cc5aa2abbb3b7db30216e9470762abd
rainleaf2012@gmail.com <rainleaf2012@gmail.com@2e08af89-c0c4-1572-9ecc-6804e04b3515>
2012-05-30 06:07:10 +0000

    添加Version和Threads

---
+++ trunk/yeti/Source/Core/YetiThreads.h
@@ -0,0 +1,15 @@
+#ifndef _CXL_YETI_THREADS_H_
+#define _CXL_YETI_THREADS_H_
+
+#include "YetiTypes.h"
+#include "YetiConstants.h"
+#include "YetiInterfaces.h"
+
+const int YETI_ERROR_CALLBACK_HANDLER_SHUTDOWN = YETI_ERROR_BASE_THREADS - 0;
+const int YETI_ERROR_CALLBACK_NOTHING_PENDING  = YETI_ERROR_BASE_THREADS - 1;
+
+NAMEBEG
+
+NAMEEND
+
+#endif // _CXL_YETI_THREADS_H_
---
+++ trunk/yeti/Source/Core/YetiVersion.h
@@ -0,0 +1,7 @@
+#ifndef _CXL_YETI_VERSION_H_
+#define _CXL_YETI_VERSION_H_
+
+#define YETI_VERSION        0x01000000
+#define YETI_VERSION_STRING "1.0.0.0"
+
+#endif // _CXL_YETI_VERSION_H_

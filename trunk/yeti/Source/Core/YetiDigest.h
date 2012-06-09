355a76344e3cc50a6037898dd1af950f9442d470
rainleaf2012@gmail.com <rainleaf2012@gmail.com@2e08af89-c0c4-1572-9ecc-6804e04b3515>
2012-06-09 07:43:56 +0000

    添加Digest功能

---
+++ trunk/yeti/Source/Core/YetiDigest.h
@@ -0,0 +1,43 @@
+#ifndef _CXL_YETI_DIGEST_H_
+#define _CXL_YETI_DIGEST_H_
+
+#include "YetiTypes.h"
+#include "YetiDataBuffer.h"
+
+NAMEBEG
+
+class Digest
+{
+public:
+    typedef enum {
+        ALGORITHM_SHA1,
+        ALGORITHM_SHA256,
+        ALGORITHM_MD5
+    } Algorithm;
+
+    static YETI_Result create(Algorithm algorithm, Digest *& digest);
+
+    virtual ~Digest() {}
+    virtual unsigned int get_size() = 0;
+    virtual YETI_Result update(const YETI_UInt8 * data, YETI_Size data_size) = 0;
+    virtual YETI_Result get_digest(DataBuffer & digest) = 0;
+
+protected:
+    Digest() {}
+};
+
+class Hmac
+{
+public:
+    static YETI_Result create(Digest::Algorithm algorithm,
+        const YETI_UInt8 * key,
+        YETI_Size key_size,
+        Digest *& digest);
+
+private:
+    Hmac() {}
+};
+
+NAMEEND
+
+#endif // _CXL_YETI_DIGEST_H_

be2a92fe2ae3e1f0b164196baa177841a6c1d565
rainleaf2012 <rainleaf2012@gmail.com>
2012-06-25 05:21:55 +0000

    添加DynamicCast功能

---
+++ yeti/Source/Core/YetiDynamicCast.h
@@ -0,0 +1,53 @@
+#ifndef _CXL_YETI_COMMON_H_
+#define _CXL_YETI_COMMON_H_
+
+#include "YetiTypes.h"
+#include "YetiCommon.h"
+#include "YetiResults.h"
+#include "YetiConfig.h"
+
+#if defined(YETI_CONFIG_NO_RTTI)
+
+#define YETI_DYNAMIC_CAST(_class, _object) \
+    ( ((_object)==0) ? 0 : reinterpret_cast<_class*>((_object)->DynamicCast(&_class::_class_##_class)) )
+#define YETI_IMPLEMENT_DYNAMIC_CAST(_class)                 \
+    static int _class_##_class;                             \
+    virtual void* DynamicCast(const void* class_anchor) {   \
+    if (class_anchor ==  &_class::_class_##_class) {        \
+    return static_cast<_class*>(this);                      \
+    }                                                       \
+    return NULL;                                            \
+}
+#define YETI_IMPLEMENT_DYNAMIC_CAST_D(_class, _supercalss)\
+    static int _class_##_class;                           \
+    virtual void* DynamicCast(const void* class_anchor) { \
+    if (class_anchor ==  &_class::_class_##_class) {      \
+    return static_cast<_class*>(this);                    \
+    } else {                                              \
+    return _superclass::DynamicCast(class_anchor);        \
+    }                                                     \
+}
+#define YETI_IMPLEMENT_DYNAMIC_CAST_D2(_class, _superclass, _mixin)\
+    static int _class_##_class;                                    \
+    virtual void* DynamicCast(const void* class_anchor) {          \
+    if (class_anchor ==  &_class::_class_##_class) {               \
+    return static_cast<_class*>(this);                             \
+    } else {                                                       \
+    void* sup = _superclass::DynamicCast(class_anchor);            \
+    if (sup) return sup;                                           \
+    return _mixin::DynamicCast(class_anchor);                      \
+    }                                                              \
+}
+#define YETI_DEFINE_DYNAMIC_CAST_ANCHOR(_class) int _class::_class_##_class = 0;
+
+#else
+
+#define YETI_DYNAMIC_CAST(_class, _object) dynamic_cast< _class * >(_object)
+#define YETI_IMPLEMENT_DYNAMIC_CAST(_class)
+#define YETI_IMPLEMENT_DYNAMIC_CAST_D(_class, _supercalss)
+#define YETI_IMPLEMENT_DYNAMIC_CAST_D2(_class, _superclass, _mixin)
+#define YETI_DEFINE_DYNAMIC_CAST_ANCHOR(_class)
+
+#endif
+
+#endif // _CXL_YETI_COMMON_H_
--- yeti/Source/Core/YetiTime.cpp
+++ yeti/Source/Core/YetiTime.cpp
@@ -4,5 +4,17 @@
 
 NAMEBEG
 
+const char * const YETI_TIME_DAYS_SHORTS[] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+const char * const YETI_TIME_DAYS_LONG[] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+const char * const YETI_TIME_MONTHS[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+
+static const YETI_Int32 YETI_TIME_MONTH_DAY[] = {-1, 30, 58, 89, 119, 150, 180, 211, 242, 272, 303, 333, 364 };
+static const YETI_Int32 YETI_TIME_MONTH_DAY_LEAP[] = {-1, 30, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365 };
+static const YETI_Int32 YETI_TIME_ELAPSED_DAYS_AT_MONTH[13] = {
+    0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365
+};
+
+const YETI_Int32 YETI_SECONDS_PER_DAY = (24L * 60L * 60L);
+const YETI_Int32 YETI_SECONDS_PER_YEAR = (365L * YETI_SECONDS_PER_DAY);
 
 NAMEEND

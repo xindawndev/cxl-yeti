8e464004602b83f919a12036391e41ae36e74aff
rainleaf2012@gmail.com <rainleaf2012@gmail.com@2e08af89-c0c4-1572-9ecc-6804e04b3515>
2012-06-29 07:51:41 +0000

    添加Socket接口

---
+++ trunk/yeti/Source/Core/YetiSocket.cpp
@@ -0,0 +1,21 @@
+#include "YetiSocket.h"
+
+#include "YetiUtil.h"
+
+NAMEBEG
+
+String SocketAddress::to_string() const
+{
+    String s = m_ipaddress_.to_string();
+    s += ':';
+    s += String::from_integer(m_port_);
+
+    return s;
+}
+
+bool SocketAddress::operator ==(const SocketAddress & other) const
+{
+    return (other.get_ipaddress().as_long() == m_ipaddress_.as_long() && other.get_port() == m_port_);
+}
+
+NAMEEND
